176317388d330f7e1e4475417b85dbfa3cfe2a47
李琳 <lilin@01.ai>
2024-11-19 18:36:41 +0800

    在useEffect中使用echarts

--- react-jike/src/pages/Home/index.js
+++ react-jike/src/pages/Home/index.js
@@ -1,5 +1,13 @@
+import BarChart from "./components/BarChart"
+
 const Home = () => {
-    return 'home'
+
+  return (
+    <div>
+      <BarChart title={'三大框架满意度'} />
+      <BarChart title={'三大框架使用度'} />
+    </div>
+  )
 }
 
 export default Home
